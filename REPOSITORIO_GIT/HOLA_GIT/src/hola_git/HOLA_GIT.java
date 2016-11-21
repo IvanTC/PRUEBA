335322618e28ec5cb6d7a84b0801f493bea86592
IvanTC <IvanTC@IvanTC-PC>
2016-11-20 21:36:24 -0500

    PRIMER EJEMPLO CON GIT Y GIT HUB

---
+++ REPOSITORIO_GIT/HOLA_GIT/src/hola_git/HOLA_GIT.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_git;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author IvanTC
+ */
+public class HOLA_GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        nombre=JOptionPane.showInputDialog("DIME TU NOMBRE: ");
+        JOptionPane.showMessageDialog(null, "HOLA "+nombre+" BIENVENIDO A LA PROGRAMACION EN GIT");
+    }
+}
+
